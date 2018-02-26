2859ed96f045ff9ad5658e2af00c673cbd4c3428
hyunseonyang <hyunseon@hyunseonyangui-MacBook-Pro.local>
2018-02-26 18:42:50 +0900

    Async Await

--- src/App.js
+++ src/App.js
@@ -6,20 +6,34 @@ class App extends Component {
 	state = {}
 
 	componentDidMount() {
-		fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
-			.then(potato => potato.json())
-			.then(json => console.log(json))
-			.catch(err => console.log(err))
+		this._getMovies();
 	}
 
 
 	_renderMovies = () => {
-		const movies = this.state.movies.map((movie, index) => {
-			return <Movie title={movie.title} poster={movie.poster} key={index} />
+		const movies = this.state.movies.map(movie => {
+			console.log(movies);
+			return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
 		})
 
 		return movies;
 	}
+
+ _getMovies = async () => {
+		const movies = await this._callApi();
+		console.log(movies)
+		this.setState({
+			movies
+		})
+	}
+
+	_callApi = () => {
+		return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+			.then(potato => potato.json())
+			.then(json => json.data.movies)
+			.catch(err => console.log(err))
+	}
+
 
   render() {
     return (
